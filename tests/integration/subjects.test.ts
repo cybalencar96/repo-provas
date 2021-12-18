d3f524dd1c3cc7ab5d6fed1c5e6c0cf9c856a5cf
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-12-18 16:49:32 -0300

    last commit wrong - adding subject tests

--- tests/integration/subjects.test.ts
+++ tests/integration/subjects.test.ts
@@ -2,7 +2,7 @@ import '../../src/setup';
 import supertest from 'supertest';
 import app from '../../src/app';
 import * as dbTest from '../dbFunctions';
-import { mockSubjectWithExam } from '../mocks/subjectMocks';
+import { mockSubjectWithExam, mockSubjectWithTeachers } from '../mocks/subjectMocks';
 
 beforeAll(async () => {
     await dbTest.init();
@@ -18,10 +18,17 @@ describe('subject ROUTES', () => {
     beforeEach(async () => population = await dbTest.populate('full'));
     afterEach(async () => await dbTest.erase());
 
-    test('should return subject array with exams', async () => {
+    test('should return subject array with exams when getSubjectsWithExams', async () => {
         const result = await supertest(app).get('/subjects');
 
         expect(result.status).toEqual(200);
         expect(result.body[0]).toEqual(mockSubjectWithExam);
+    });
+
+    test('should return subject array when getMany', async () => {
+        const result = await supertest(app).get('/subjects/where');
+
+        expect(result.status).toEqual(200);
+        expect(result.body[0]).toEqual(mockSubjectWithTeachers);
     });
 });
--- tests/mocks/subjectMocks.ts
+++ tests/mocks/subjectMocks.ts
@@ -7,6 +7,14 @@ const mockSubjectWithExam = {
 		exams: [mockExam]
 }
 
+const mockSubjectWithTeachers = {
+	id: expect.any(Number),
+	name: expect.any(String),
+	period: expect.any(Number),
+	teachers: expect.any(Array),
+}
+
 export {
     mockSubjectWithExam,
+	mockSubjectWithTeachers,
 }
