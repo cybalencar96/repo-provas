47bd035f1a92e916149488399462efeb32eff1f3
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-12-19 15:41:06 -0300

    update dump

--- dump.sql
+++ dump.sql
@@ -13,6 +13,7 @@ CREATE TABLE "exams" (
 	"name" varchar(255) NOT NULL,
 	"category" varchar(255) NOT NULL,
 	"class_id" int NOT NULL,
+	"link_pdf" varchar(255) NOT NULL,
 	CONSTRAINT "exams_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -23,7 +24,7 @@ CREATE TABLE "exams" (
 CREATE TABLE "subjects" (
 	"id" serial NOT NULL,
 	"name" varchar(255) NOT NULL,
-	"period" varchar(255) NOT NULL,
+	"period" integer NOT NULL,
 	CONSTRAINT "subjects_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -53,15 +54,33 @@ ALTER TABLE "classes" ADD CONSTRAINT "classes_fk1" FOREIGN KEY ("subject_id") RE
 
 INSERT INTO teachers (name) VALUES ('Mr James');
 INSERT INTO teachers (name) VALUES ('Ms Alice');
+INSERT INTO teachers (name) VALUES ('Luiz de Redes');
+INSERT INTO teachers (name) VALUES ('Arnald Bisson');
+INSERT INTO teachers (name) VALUES ('Ana Claudia');
 INSERT INTO subjects (name, period) VALUES ('Matematica', 5);
 INSERT INTO subjects (name, period) VALUES ('Portugues', 3);
 INSERT INTO subjects (name, period) VALUES ('Ciencias', 1);
+INSERT INTO subjects (name, period) VALUES ('Controle Linear', 10);
+INSERT INTO subjects (name, period) VALUES ('Fundacoes', 7);
+INSERT INTO subjects (name, period) VALUES ('Programação', 5);
 INSERT INTO classes (teacher_id, subject_id) VALUES (3,5);
 INSERT INTO classes (teacher_id, subject_id) VALUES (4,4);
-INSERT INTO classes (teacher_id, subject_id) VALUES (4,6);
-INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.1', 'P1', 4, 'www.google.com');
-INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.1', 'P2', 5, 'www.google.com');
-INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2019.1', 'PF', 6, 'www.google.com');
-INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2019.1', '2ch', 6, 'www.google.com');
+INSERT INTO classes (teacher_id, subject_id) VALUES (4,1);
+INSERT INTO classes (teacher_id, subject_id) VALUES (5,5);
+INSERT INTO classes (teacher_id, subject_id) VALUES (5,4);
+INSERT INTO classes (teacher_id, subject_id) VALUES (4,4);
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.1', 'P1', 1, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2021.1', 'PF', 2, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2021.2', 'PF', 2, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2021.2', '2ch', 2, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2019.1', 'P1', 7, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2019.1', 'P2', 7, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2012.1', '2ch', 4, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2016.2', 'P1', 5, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2018.1', '2ch', 6, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2018.1', 'P3', 6, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.1', 'P1', 6, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.2', '2ch', 6, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
+INSERT INTO exams (name, category, class_id, link_pdf) VALUES ('2022.2', '2ch', 8, 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf');
 
 
